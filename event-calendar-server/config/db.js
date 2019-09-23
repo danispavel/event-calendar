72a75567861b2427f080b4aeef03f12d1e3587bf
Pavlo Danis <danispavlolll@gmail.com>
2019-09-23 17:03:53 +0300

    add config

---
+++ event-calendar-server/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url:
+    "mongodb://admin:admin@cluster0-shard-00-00-hgihw.mongodb.net:27017,cluster0-shard-00-01-hgihw.mongodb.net:27017,cluster0-shard-00-02-hgihw.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+};
